623138f27f84dc4afe736c3a26960ecb5563ca1d
guanzhou-zhao <benzhao2866@gmail.com>
2016-08-16 21:12:17 +1200

    Add pagination buttons

--- twitch_tv.js
+++ twitch_tv.js
@@ -35,6 +35,31 @@ var load_sublist = function(evt) {
         dataType: "json",
         success: function(data) {
             console.log(data)
+// make pagination
+// Hidden pagination if there is no next and prev links
+        var pagination = $(".p_main nav")
+        if (data._links.next || data._links_prev) {
+          pagination.show()
+          var prev_link = $(".p_main nav .pager:first-child")
+          var next_link = $(".p_main nav .pager:last-child")
+          if (data._links.prev) {
+            prev_link.show()
+            prev_link.children().first().attr("href", data._links_prev).click(load_sublist)
+          } else {
+            prev_link.hide()
+          }
+
+          if (data._links.next) {
+            next_link.show()
+            next_link.children().first().attr("href", data._links_next).click(load_sublist)
+          } else {
+            next_link.hide()
+          }
+        } else {
+          pagination.hide()
+        }
+
+//Generate html for loaded data
             var col_pre_row = 3;
             var row = $("<div>").addClass("row")
             data.streams.reduce(function(preV, stream) {
